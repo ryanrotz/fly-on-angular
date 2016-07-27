bd21b5ac015d29dd494bb56b5bc424f2d6288d26
Ryan Rotz <ryanrotz@gmail.com>
2016-07-26 18:51:50 -0700

    set up static files and app.js

--- index.js
+++ index.js
@@ -6,11 +6,17 @@ var app = express();
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/airplanes');
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(require('morgan')('dev'));
 
 app.use('/api/airplanes', require('./controllers/airplane'));
+
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
 var server = app.listen(process.env.PORT || 3000);
 
